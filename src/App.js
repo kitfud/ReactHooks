e63b8c9c4478d36151b3a00560dc5feb19fadddb
Kit Fuderich <kitfuderich@gmail.com>
2020-11-19 11:20:43 -0800

    state hook array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import StateHook from './components/StateHook.js'
+//import StateHook from './components/StateHook.js'
+//import StateHookObject from './components/StateHookObject'
+import StateHookArray from './components/StateHookArray'
+
 function App() {
   return (
     <div> 
-      <StateHook></StateHook>
+     <StateHookArray></StateHookArray>
     </div>
   );
 }
